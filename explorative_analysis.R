87a15082923770ec1d750bd30abd441026c486da
Wisse Barkhof <wisse-barkhof@hotmail.com>
2018-10-25 15:28:26 +0200

    Revert "added data"
    This reverts commit 57ad0b7e491a8060668a354354cb2f6179898f5d.

--- explorative_analysis.R
+++ explorative_analysis.R
@@ -1,5 +1,5 @@
 # Load Data
-globaltemp <- read.csv('data/raw_temp_emissions_1960_2014.csv', header = TRUE, sep = ",")
+globaltemp <- read.csv('data/temp_emissions_1960_2014.csv', header = TRUE, sep = ",")
 summary(globaltemp)
 
 # Library from https://otexts.org/fpp2/
@@ -12,7 +12,7 @@ for (col in datecols) {
   globaltemp[col] <- ts(globaltemp[col], start=1960, frequency=12)
 }
 
-globaltemp$world_emission <- ts(globaltemp$world_emission, start=1960, frequency=12)
+globaltemp$world_emission <- ts(globaltemp$world_emission, start=1960, frequency=1)
 
 # Repeating every yearly entry for world-emission per month
 em <-  globaltemp$world_emission
@@ -28,30 +28,22 @@ for (i in 1:length(em)) {
 
 globaltemp$world_emission_month_rep <- ts(em, start = 1960, frequency = 12)
 
-# create a ts for interpolation
-em_int <- ts(globaltemp$world_emission, start = 1960, frequency = 12)
-# add value at the end to ensure interpolation continues until 12/2014 (repeat value for 01/2014)
-em_int <- rbind(em_int, ts(36138285, start=2015))
 
 # interpolate missing values
+em_int <- globaltemp$world_emission
+
 library(zoo)
-Cz <- zoo(em_int)
+Cz <- zoo(em_int, frequency = 1)
 Cz_approx <- ts(na.approx(Cz), start=1960, frequency = 12)
-
-# check
 plot(Cz_approx)
-
-# cut off 01/2015 value to match other ts objects
-globaltemp$world_emission_month_int <- window(Cz_approx, start=1960, end=c(2014, 12))
+# globaltemp$world_emission_month_int <- Cz_approx
 
 globaltemp <- ts(globaltemp, start = 1960, frequency = 12)
-globaltemp_train <- window(globaltemp, 1960, c(2004, 12))
-globaltemp_test <- window(globaltemp, c(2005, 01))
+globaltemp_train <- window(globaltemp, 1960, 2004)
+globaltemp_test <- window(globaltemp, 2004)
 
-write.csv(globaltemp, "data/temp_emissions_1960_2014.csv")
-write.csv(globaltemp_train, "data/temp_emissions_1960_2004.csv")
-write.csv(globaltemp_test, "data/temp_emissions_2005_2014.csv")
-
+write.csv(globaltemp_train, "temp_emissions_1960_2004.csv")
+write.csv(globaltemp_test, "temp_emissions_2004_2014.csv")
 
 # Plot Time Series
 autoplot(globaltemp$LandAndOceanAverageTemperature) +
@@ -67,7 +59,7 @@ ggseasonplot(globaltemp$LandAndOceanAverageTemperature, year.labels=TRUE, year.l
 # Why is there seasonality in avg GLOBAL temperature?
 
 # Seasonal Subseries Plot
-ggsubseriesplot(globaltemp[,'LandAndOceanAverageTemperature']) +
+ggsubseriesplot(globaltemp$LandAndOceanAverageTemperature) +
   ylab("Celsius") +
   ggtitle("Seasonal Subseries plot: Average Land Temperature")  
 
@@ -76,21 +68,19 @@ qplot(LandAverageTemperature, world_emission, data=as.data.frame(globaltemp)) +
   ylab("World Emission") + xlab("Average Temperature")     
 
 # Some naive methods
-naive_fc <- naive(globaltemp[,'LandAndOceanAverageTemperature'], 120)
+naive_fc <- naive(globaltemp$LandAndOceanAverageTemperature, 120)
 autoplot(naive_fc) +
   ylab("Celsius") +
   ggtitle("Naive Average Land Temperature")  
   
 # Taking account for seasonality
-snaive_fc <- snaive(globaltemp[,'LandAndOceanAverageTemperature'], 120)
+snaive_fc <- snaive(globaltemp$LandAndOceanAverageTemperature, 120)
 autoplot(snaive_fc) +
   ylab("Celsius") +
   ggtitle("Seasonal Naive Average Land Temperature")  
 
 # Simple Exponential Smoothing
-(fc <- ses(globaltemp[,'LandAndOceanAverageTemperature'], h=12))
+(fc <- ses(globaltemp$LandAndOceanAverageTemperature, h=12))
 autoplot(fc) +
   autolayer(fitted(fc), series="Fitted") +
   ylab("Celsius") + xlab("Year")
-
-
--- merge_monthly_co2.R
+++
@@ -1,8 +0,0 @@
-# CO2 Emssion
-co2 <- ts(read.csv('data/mole_fraction_of_carbon_dioxide_in_air_input4MIPs_GHGConcentrations_CMIP_UoM-CMIP-1-1-0_gr3-GMNHSH_000001-201412.csv'), start = 0000, frequency = 12)
-co2 <- window(co2, start = 1960)
-
-temp <- read.csv('data/temp_emissions_1960_2014.csv')
-temp['montly_co2'] <- co2[, 'data_mean_global']
-
-write.csv(temp, 'data/montly_temp_emissions_1960_2014.csv')
